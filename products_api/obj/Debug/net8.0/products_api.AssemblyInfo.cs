24a607cb85792fb807ae4597d1735403671d8b01
Clayton Rocha <clayton.will08@gmail.com>
2025-01-30 16:02:10 -0300

    Renomeação dos arquivos

--- products_api/obj/Debug/net8.0/products_api.AssemblyInfo.cs
+++ products_api/obj/Debug/net8.0/products_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("products_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f8794260b38b9b20bfa4e67cfd9cc9fc651105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d9eb66299c2f286251bb5f1ab9144ccb406384")]
 [assembly: System.Reflection.AssemblyProductAttribute("products_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("products_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
